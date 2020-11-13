35e78c5a0c65d4c3532229ee82c06226be4b6505
Javier Nieto Lorenzo <58864261+JavierNLSauces@users.noreply.github.com>
2020-11-13 10:49:59 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 07 - Javier Nieto Lorenzo</title>
+    </head>
+    <body>
+        <h1>AplicacionComunEquipo1</h1>
+        <a href="cristina.php">Enlace a index Cristina</a>
+    </body>
+</html>
+
