8323eb65d171c0d6527edfb484fd6c41ce937808
Cristina Nuñez Sebastian <cristina.nunseb@educa.jcyl.es>
2020-11-13 10:53:38 +0100

    Add cristina.php

---
+++ cristina.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicio 01</title>         
+    </head>
+    <body>
+        <h1>Index de Cristina</h1>
+    </body>
+</html>     
